b7bcdad112005cca81b9ddd782fe46edfffce7de
Lebon Fabien <fabienlebon974@gmail.com>
2018-11-18 18:49:37 +0100

    Single Game CardView
    CardView d'un jeu dans components, a importer dans screens/jeux.js possibilité d'afficher n jeux avec des paramètres {Nom du jeu, image du jeu, nombre d'evenements relatifs au jeu}

--- app/components/jeux/index.js
+++ app/components/jeux/index.js
@@ -1,27 +1,59 @@
 //import liraries
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { ImageBackground } from 'react-native'
+import PropTypes from 'proptypes'
+
+import styled from 'styled-components'
+
+
+
+const GameName = styled.Text `
+height: 20%; 
+color: white;
+fontWeight: bold;
+backgroundColor: black;
+`
+
+const TextViewOnImage = styled.View `
+flex: 1;
+flexDirection: row;
+justifyContent: space-between;
+borderWidth: 1;
+borderColor: black;
+`
+
+const CardView = styled.View `
+width: 80%;
+height: 10%;
+borderWidth: 1;
+borderColor: black;
+borderRadius: 5;
+`
 
 // create a component
 class jeux extends Component {
+    static propTypes = {
+        GameName: PropTypes.string,
+        TotalNumberEvent: PropTypes.number,
+        SourceImage: PropTypes.string,
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text>jeux</Text>
-            </View>
+            <CardView>
+                <ImageBackground source={this.props.SourceImage} style={{width: '100%', height: '100%'}} >
+                    <TextViewOnImage>
+                        <GameName>{this.props.GameName}</GameName>
+                        <GameName>{this.props.TotalNumberEvent}</GameName>
+                    </TextViewOnImage>
+                </ImageBackground>
+
+            </CardView>
         )
     }
 }
 
-// define your styles
-const styles = StyleSheet.create({
-    container: {
-        flex: 5,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#FFF',
-    },
-})
+
 
 //make this component available to the app
 export default jeux
--- app/screens/jeux.js
+++ app/screens/jeux.js
@@ -1,9 +1,16 @@
 //import liraries
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { BackgroundView } from '../static/customStyle/formStyled'
+import { BackgroundView, CenterView } from '../static/customStyle/formStyled'
 import Jeux from '../components/jeux'
+import styled from 'styled-components'
 
+import CsgoImage from '../static/images/csgo.jpg'
+import LolImage from '../static/images/lol.jpg'
+
+const Separator =  styled.View`
+marginTop: 10px;
+`
 
 // create a component
 class JeuxScreen extends Component {
@@ -13,7 +20,11 @@ class JeuxScreen extends Component {
     render() {
         return (
             <BackgroundView>
-                <Jeux/>
+                <CenterView>
+                    <Jeux SourceImage={CsgoImage} GameName="CS GO" TotalNumberEvent="20"  />
+                    <Separator/>
+                    <Jeux SourceImage={LolImage} GameName="LOL" TotalNumberEvent="8"  />
+                </CenterView>
             </BackgroundView>
         )
     }
